6eee33abfd4468c554a3e76911b2268409882491
nicoledbianchin <nickdiasbianchin@hotmail.com>
2020-02-03 20:34:23 -0300

    relacionamento um para um

---
+++ src/orientacaoAObjetos/composicao/Motor.java
@@ -0,0 +1,16 @@
+package orientacaoAObjetos.composicao;
+
+public class Motor {
+
+    boolean ligado = false;
+    double fatorInjecao = 1;
+
+    int giros() {
+        if(!ligado){
+            return 0;
+        }
+        else {
+            return (int) fatorInjecao * 3000;
+        }
+    }
+}
