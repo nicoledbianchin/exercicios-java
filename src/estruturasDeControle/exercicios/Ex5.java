3dcfcdaefba238dd9ac057c98372d01c84928188
nicoledbianchin <nicoledbianchin@gmail.com>
2020-02-28 17:21:36 -0300

    ex5 estruturas de controle

---
+++ src/estruturasDeControle/exercicios/Ex5.java
@@ -0,0 +1,33 @@
+package estruturasDeControle.exercicios;
+
+import java.util.Scanner;
+
+public class Ex5 {
+    //Refatorar o exercício 04, utilizando a estrutura switch.
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe um número: ");
+        int numero = entrada.nextInt();
+
+        int contadorDivisores = 0;
+        for (int i= 2; i < numero; i++){
+            if (numero % i == 0) {
+                contadorDivisores++;
+                }
+            }
+
+        switch (contadorDivisores) {
+            case 0: {
+                System.out.println("Número primo");
+                break;
+            }
+            default: {
+                System.out.println("Número não primo");
+            }
+        }
+
+        entrada.close();
+    }
+}
