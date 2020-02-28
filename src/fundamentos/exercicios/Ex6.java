817af3ad03cb35bf8fce6574e1c23f704b6a429e
nicoledbianchin <nicoledbianchin@gmail.com>
2020-02-28 15:52:12 -0300

    ex6 fundamentos

---
+++ src/fundamentos/exercicios/Ex6.java
@@ -0,0 +1,26 @@
+package fundamentos.exercicios;
+
+import java.util.Scanner;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe o 'a': ");
+        double a = entrada.nextDouble();
+
+        System.out.println("Informe o 'b': ");
+        double b = entrada.nextDouble();
+
+        System.out.println("Informe o 'c': ");
+        double c = entrada.nextDouble();
+
+        double delta = Math.pow(b, 2) - (4 * a * c);
+        double x1 = (- b + delta) / (2 * a);
+        double x2 = (- b - delta) / (2 * a);
+
+        System.out.println("X1: " + x1 + ", X2: " + x2 + ".");
+
+        entrada.close();
+    }
+}
