aca39af0003bf308fcfa22ca0a8eb372781a60f6
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-04 14:28:03 -0300

    retira linha inutil

--- src/orientacaoAObjetos/composicao/desafio/Produto.java
+++ src/orientacaoAObjetos/composicao/desafio/Produto.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Produto {
     String nome;
     double preco;
-    ArrayList<Item> itens = new ArrayList<>();
 
     Produto (String nome, double preco) {
         this.nome = nome;
