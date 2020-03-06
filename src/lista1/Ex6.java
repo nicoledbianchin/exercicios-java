5ce9bc5d9704759172c2ffc4e43e35bb5f75634e
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-06 10:14:48 -0300

    ex6

--- src/lista1/Ex6.java
+++ src/lista1/Ex6.java
@@ -1,4 +1,20 @@
 package lista1;
 
+import java.util.Scanner;
+
 public class Ex6 {
+//    6. Desenvolva um algoritmo em Java que leia um número inteiro e imprima o seu
+//    antecessor e seu sucessor.
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Número: ");
+        int numero = entrada.nextInt();
+
+        System.out.println(numero - 1);
+        System.out.println(numero + 1);
+
+        entrada.close();
+    }
 }
