55182edeb53a26d10b3a040f6e1596feff3b7498
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-06 09:49:22 -0300

    ex1

--- src/lista1/Ex1.java
+++ src/lista1/Ex1.java
@@ -1,4 +1,25 @@
 package lista1;
 
+import java.util.Scanner;
+
 public class Ex1 {
+//    1- Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e
+//    mostre-a expressa em dias. Leve em consideração o ano com 365 dias e o mês com 30.
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int idade = 0;
+        System.out.println("Informe sua idade, começando pelo ano: ");
+        idade = idade + (entrada.nextInt() * 365);
+
+        System.out.println("Agora o mês: ");
+        idade = idade + (entrada.nextInt() * 30);
+
+        System.out.println("E por fim os dias: ");
+        idade = idade + entrada.nextInt();
+
+        System.out.println("Sua idade em dias é: " + idade);
+
+        entrada.close();
+    }
 }
