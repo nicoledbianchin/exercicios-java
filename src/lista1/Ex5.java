a22e2b5cd007971b681e7bf3842f81ca71b9d0f8
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-06 10:14:37 -0300

    ex5

--- src/lista1/Ex5.java
+++ src/lista1/Ex5.java
@@ -1,4 +1,25 @@
 package lista1;
 
+import java.util.Scanner;
+
 public class Ex5 {
+//    5. Crie um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário,
+//    calcule a quantidade de salários mínimos esse usuário ganha e imprima o resultado.
+//            (1SM=R$788,00)
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Salário mínimo: ");
+        double salarioMinimo = entrada.nextDouble();
+
+        System.out.println("Salário usuário: ");
+        double salario = entrada.nextDouble();
+
+        double resultado = salario / salarioMinimo;
+
+        System.out.println(resultado);
+
+        entrada.close();
+    }
 }
