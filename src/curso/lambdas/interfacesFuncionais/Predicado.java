15c14a0378af22675c70af6df27c93324b4bcfea
nicoledbianchin <nicoledbianchin@gmail.com>
2020-06-18 09:10:36 -0300

    predicate

---
+++ src/curso/lambdas/interfacesFuncionais/Predicado.java
@@ -0,0 +1,15 @@
+package curso.lambdas.interfacesFuncionais;
+
+import java.util.function.Predicate;
+
+public class Predicado {
+    public static void main(String[] args) {
+
+        // recebe um parametro e retorna um boolean
+        Predicate<Produto> isCaro =
+                produto -> (produto.preco * (1 - produto.desconto)) >= 750;
+
+        Produto produto = new Produto("Notebook", 3893.89, 0.15);
+        System.out.println(isCaro.test(produto));
+    }
+}
